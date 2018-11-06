1a879d4dd653ef93945e7ad96894460ec6bae184
abhijeet iyer <abhijeet.iyer15@siesgst.ac.in>
2018-11-06 10:45:07 +0530

    .

---
+++ scripts/iete.js
@@ -0,0 +1,56 @@
+document.addEventListener('DOMContentLoaded', function(){
+  let wrapper = document.getElementById('wrapper');
+  let topLayer = wrapper.querySelector('.top');
+  let handle = wrapper.querySelector('.handle');
+  let skew = 0;
+  let delta = 0;
+
+  if(wrapper.className.indexOf('skewed') != -1){
+    skew = 955;
+  }
+  
+  wrapper.addEventListener('mousemove', function(e){
+    delta = (e.clientX - window.innerWidth / 2) * 0.5;
+  
+    handle.style.left = e.clientX + delta + 'px';
+
+    topLayer.style.width = e.clientX + skew + delta + 'px';
+  });
+});
+
+//Select all links with hashes
+$('a[href*="#"]')
+// Remove links that dont actually link to anything
+  .not('[href="#"]')
+  .not('[href="#0"]')
+  .click(function(event) {
+    // On-page links
+    if (
+      location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') 
+      && 
+      location.hostname == this.hostname
+    ) {
+      // Figure out element to scroll to
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+      // Does a scroll target exist?
+      if (target.length) {
+        // Only prevent default if animation is actually gonna happen
+        event.preventDefault();
+        $('html, body').animate({
+          scrollTop: target.offset().top
+        }, 1000, function() {
+          // Callback after animation
+          // Must change focus!
+          var $target = $(target);
+          $target.focus();
+          if ($target.is(":focus")) { // Checking if the target was focused
+            return false;
+          } else {
+            $target.attr('tabindex','-1'); // Adding tabindex for elements not focusable
+            $target.focus(); // Set focus again
+          };
+        });
+      }
+    }
+  });

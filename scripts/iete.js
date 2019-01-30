079acef12e2673362ad6507bd61db9ec24a3ada7
unknown <abhijeet.iyer15@siesgst.ac.in>
2019-01-30 07:47:23 +0530

    thirteen

--- scripts/iete.js
+++ scripts/iete.js
@@ -1,22 +1,3 @@
-document.addEventListener('DOMContentLoaded', function(){
-  let wrapper = document.getElementById('wrapper');
-  let topLayer = wrapper.querySelector('.top');
-  let handle = wrapper.querySelector('.handle');
-  let skew = 0;
-  let delta = 0;
-
-  if(wrapper.className.indexOf('skewed') != -1){
-    skew = 1000;
-  }
-  
-  wrapper.addEventListener('mousemove', function(e){
-    delta = (e.clientX - window.innerWidth / 2) * 0.5;
-  
-    handle.style.left = e.clientX + delta + 'px';
-
-    topLayer.style.width= e.clientX + skew + delta + 'px';
-  });
-});
 
 //Select all links with hashes
 $('a[href*="#"]')
@@ -54,3 +35,22 @@ $('a[href*="#"]')
       }
     }
   });
+document.addEventListener('DOMContentLoaded', function(){
+  let wrapper = document.getElementById('wrapper');
+  let topLayer = wrapper.querySelector('.top');
+  let handle = wrapper.querySelector('.handle');
+  let skew = 0;
+  let delta = 0;
+
+  if(wrapper.className.indexOf('skewed') != -1){
+    skew = 1000;
+  }
+  
+  wrapper.addEventListener('mousemove', function(e){
+    delta = (e.clientX - window.innerWidth / 2) * 0.5;
+  
+    handle.style.left = e.clientX + delta + 'px';
+
+    topLayer.style.width= e.clientX + skew + delta + 'px';
+  });
+});
